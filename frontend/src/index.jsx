c209aaad2196cb6da633e7987629b65763535206
Charles Thomas <cjthom03@gmail.com>
2022-04-05 19:40:30 -0700

    add config for MUI; add default mui stylesheet

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import CssBaseline from '@mui/material/CssBaseline';
 
-import './default.css';
 import App from './App';
 
 const appRoutes = (
-  <Router>
-    <Routes>
-      <Route path="/" element={<App />} />
-    </Routes>
-  </Router>
+  <React.Fragment>
+    <CssBaseline />
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+      </Routes>
+    </Router>
+  </React.Fragment>
 );
 
 ReactDOM.render(appRoutes, document.getElementById('root'));
