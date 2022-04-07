c3d6b034e2121a0f6a236f8896354b40cf1ea7c9
Charles Thomas <cjthom03@gmail.com>
2022-04-06 17:34:02 -0700

    add a dark mode switch

--- frontend/src/Components/Header/Header.tsx
+++ frontend/src/Components/Header/Header.tsx
@@ -1,8 +1,18 @@
 import * as React from 'react';
 import styled from 'styled-components';
-import { Typography  } from '@mui/material';
+import { Typography, Switch as MuiSwitch  } from '@mui/material';
 
 export const HEIGHT = 50;
+
+const Wrapper = styled.div(({ theme }) => ({
+  height: HEIGHT,
+  width: '100%',
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+  textAlignLast: 'center',
+  borderBottom: `1px solid ${theme.palette.border.grey}`
+}));
 
 const Title = styled(Typography).attrs(() => ({
   variant: 'h4',
@@ -10,21 +20,43 @@ const Title = styled(Typography).attrs(() => ({
   color: theme.palette.text.primary,
   fontSize: '2.3125rem',
   fontFamily: 'KarnakProCondensed',
+  width: '60%',
 }))
 
-const Wrapper = styled.div(({ theme }) => ({
-  height: HEIGHT,
-  width: '100%',
+const ThemeToggle = styled.div(() => ({
+  width: '20%',
   display: 'flex',
-  justifyContent: 'center',
-  alignItems: 'center',
-  borderBottom: `1px solid ${theme.palette.border.grey}`
+  justifyContent: 'flex-end',
+  alignItems: 'center'
+}));
+
+const Label = styled(Typography)``;
+
+const Switch = styled(MuiSwitch)(({theme}) =>({
+  '& .MuiSwitch-switchBase': {
+    '&.Mui-checked': {
+      color: theme.palette.common.white,
+      '& + .MuiSwitch-track': {
+        backgroundColor: theme.palette.fill.green,
+        opacity: 1,
+      }
+    }
+  }
+}));
+
+const PlaceHolder = styled.div(() => ({
+  width: '20%',
 }));
 
 export const Header = () => {
   return (
     <Wrapper>
+      <PlaceHolder />
       <Title>Wordle</Title>
+      <ThemeToggle>
+        <Label variant='body2'>Dark Mode</Label>
+        <Switch disableRipple />
+      </ThemeToggle>
     </Wrapper>
   )
 };
