61daa5a17ae28e74210fe6e578339849b96712c9
Charles Thomas <cjthom03@gmail.com>
2022-04-15 16:18:16 -0700

    add a space

--- frontend/src/Helpers/styleHelpers.ts
+++ frontend/src/Helpers/styleHelpers.ts
@@ -75,7 +75,7 @@ const Shake = keyframes`
   10%, 90% { transform: translateX(-1px); }
   20%, 80% { transform: translateX(2px); }
   30%, 50%, 70% { transform: translateX(-4px); }
-  40%,60% { transform: translateX(4px); }
+  40%, 60% { transform: translateX(4px); }
 `
 
 const ShakeAnimation = css`
